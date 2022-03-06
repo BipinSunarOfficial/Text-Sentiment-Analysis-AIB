754630bc00579031783f906bef1692bb955db704
Bipin <vc74it12@vedascollege.edu.np>
2022-03-06 17:37:02 +0545

    Sentiment Analysis

--- base/ml.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-train = pd.read_csv(r'C:\Users\Bipin\Desktop\Sentiment analysis\TSA\data\test\test.csv', encoding='utf-8')
-train.head()
-test = pd.read_csv(r'C:\Users\Bipin\Desktop\Sentiment analysis\TSA\data\train\train.csv', encoding='utf-8')
-test.head()
-train[train['label'] == 0].head(10)
--- base/views.py
+++ base/views.py
@@ -1,44 +1,33 @@
-from django.shortcuts import render , HttpResponse
+import array
+from django.shortcuts import render
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+import pickle
 import numpy as np
-import pandas as pd
-import re #for regular expressions
-import nltk #for text manipulation
-import string
-import warnings
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 
 def home(request):
-    return render(request,('base/home.html'))
+    return render(request, "base/home.html")
 
 
-def nb(request):
+def getSentiment(request):
     if request.method == 'POST':
-        text = request.POST['search']
-        pd.set_option("display.max_colwidth",200)
-        warnings.filterwarnings("ignore",category=DeprecationWarning)
-        train =pd.read_csv(r'C:\Users\Bipin\Desktop\Sentiment analysis\TSA\data\train\train.csv', encoding='utf-8')
-        train
-        test = pd.read_csv(r'C:\Users\Bipin\Desktop\Sentiment analysis\TSA\data\test\test.csv', encoding='utf-8')
-        test
-        train[train['label'] == 0].head(10)
-        train[train['label'] == 1].head(10)
-        train.shape
-        test.shape
-        train['label'].value_counts()
-        length_train_dataset = train['tweet'].str.len()
-        length_test_dataset = test['tweet'].str.len()
-        # plt.hist(length_train_dataset, bins=20,label="Train tweets")
-        # plt.hist(length_test_dataset, bins=20,label="Test tweets")
-        # plt.legend() 
-        # plt.show()
-        combine=train.append(test,ignore_index=True) #train and test dataset are combined
-        combine.shape
-def remove_pattern(input_text,pattern):
-        r= re.findall(pattern, input_text)
-        for i in r:
-        input_text = re.sub(i, '', input_text)
-        return input_text
+        twt = request.POST['txtarea']
+        text = twt
+        twt = [twt]
+        with open('model/sentiment.pickle', 'rb') as handle:
+            loaded_tokenizer = pickle.load(handle)
 
-# Create your views here.
+        seq = loaded_tokenizer.texts_to_sequences(twt)
+        padded = pad_sequences(seq, maxlen=28, dtype='int32', value=0)
+
+        model = load_model('model/sentiment.h5')
+        sentiment = model.predict(padded, batch_size=1)[0]
+        # prob = model.predict(array([np.vectorize][0]))[0][0]
+        if(np.argmax(sentiment) == 0):
+            return render(request, "base/home.html", {'result': 'Sentiment is Negative. 😌 '})
+        elif (np.argmax(sentiment) == 1):
+            return render(request, "base/home.html", {'result': 'Sentiment is Positive. 😃'})
